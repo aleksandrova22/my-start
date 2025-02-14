2bbb3ca8fb8ee599308911b2caceb1bf4b841580
aleksandrova22 <lialia1986@mail.ru>
2025-02-14 18:22:48 +0700

    1402

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,23 @@
-export default function Page2() {
-    return <h1>Обо мне</h1>
-  }
+
+import { auth } from '@/auth';
+import { GetUsers } from '@/components/Elem/users';
+//import {ServerSession } from '@/components/my-account';
+import { prisma } from '@/prisma/prisma';
+
+export default async function Page2() {
+  const
+    session = await auth();
+  if (!session?.user) return <div>Список всех пользователей будет доступен после авторизации</div>;
+
+  const
+    users = await prisma.user.findMany();
+  if ((session.user.email = 'lialia1986@mail.ru') || (session.user.email = 'materikin@gmail.com'))
+
+    return <>
+      <h1>Админ</h1>
+          <hr />
+          <h3>Перечень всех пользователей - доступен для админов</h3>
+      {/* Server session = <ServerSession /> */}
+      <div> <GetUsers users={users} /> </div>
+    </>
+}
---
+++ components/Elem/users.tsx
@@ -0,0 +1,14 @@
+import type { User } from '@prisma/client';
+
+
+
+export function GetUsers({ users }: { users: User[] }) {
+      return <ul >
+            {users?.map(user => <li key={user.id}>
+                  {user.name}<br />
+                  {user.email}<br />
+                    <img src={user.image} width={50} height={50} /> 
+            </li>)}
+      </ul>
+
+}
